fddc9835a330c1de891b2407ff83a3b2e0a4f71b
apurva <apurva@optessa.com>
2024-12-20 23:37:07 +0530

    restructured folders

--- service/migrations/create_enum.sql
+++
@@ -1,3 +0,0 @@
-CREATE TYPE source_enum AS ENUM ('F', 'B');
-CREATE TYPE status_enum AS ENUM ('pending', 'processed');
-CREATE TYPE result_enum AS ENUM ('success', 'failed');
---
+++ service/migrations/create_table.sql
@@ -0,0 +1,41 @@
+CREATE TYPE source_enum AS ENUM ('F', 'B');
+CREATE TYPE status_enum AS ENUM ('pending', 'processed');
+CREATE TYPE result_enum AS ENUM ('success', 'failed');
+
+CREATE TABLE events (
+            id SERIAL PRIMARY KEY,
+            uuid VARCHAR(255) NOT NULL,
+            source source_enum NOT NULL,
+            url VARCHAR(255) NOT NULL,
+            payload TEXT,
+            status status_enum NOT NULL,
+            created_at TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP NOT NULL,
+            result result_enum NOT NULL,
+            user_agent VARCHAR(255),
+            ad_blocker_active BOOLEAN NOT NULL,
+            plugin_installed VARCHAR(255)
+);
+
+CREATE TABLE error_logs (
+            id SERIAL PRIMARY KEY,
+            uuid VARCHAR(255) NOT NULL,
+            log TEXT,
+            title VARCHAR(255),
+            status status_enum NOT NULL,
+            created_at TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP NOT NULL,
+            source source_enum NOT NULL
+);
+
+CREATE TABLE analysis (
+            id SERIAL PRIMARY KEY,
+            uuid VARCHAR(255) NOT NULL,
+            event_id INT NOT NULL,
+            insights VARCHAR(255),
+            fixable BOOLEAN DEFAULT FALSE,
+            remarks TEXT,
+            created_at TIMESTAMP NOT NULL,
+            updated_at TIMESTAMP NOT NULL,
+            FOREIGN KEY (event_id) REFERENCES events (id)
+);
